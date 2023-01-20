8e4e5d84b6c130af96cd1a30cba02d993faa1164
Tetiana Kharchenko <t.kharchenko@quin.md>
2023-01-20 17:05:37 +0100

    product page switcher

--- js/data.mjs
+++ js/data.mjs
@@ -3,43 +3,58 @@ const data = [
     title: 'Black Crew Neck T-shirt',
     price: '100$',
     image: 'images/home-images/Black-man.png',
+    id: '1',
+    variants: [{
+        name: 'black',
+        image: 'images/home-images/Black-man.png',
+    }, {
+        name: 'pink',
+        image: 'images/home-images/Pink-man.png',
+    }]
 },
 {
     title: 'Black Crew Neck T-shirt',
     price:'100$',
     image: 'images/home-images/Black-woman.png',
+    id: '2',
 },
 {
     title: 'Pink Crew Neck T-shirt',
     price:'100$',
     image: 'images/home-images/Pink-man.png',
+    id: '3',
 },
 {
     title: 'Pink Crew Neck T-shirt',
     price:'100$',
     image: 'images/home-images/Pink-woman.png',
+    id: '4',
 },
 
 {
     title: 'Slim Woman Sportswear',
     price:'200$',
     image: 'images/home-images/Woman-sportswear.png',
+    id: '5',
 },
 {
     title: 'Woman`s Gray Hoodie',
     price: '150$',
     image: 'images/home-images/Womans-gray-hoodie.png',
+    id: '2',
 },
 {
     title: 'Woman Dress With Stripes',
     price: '180$',
     image: 'images/home-images/Woman-dress-with-stripes.png',
+    id: '6',
 },
 {
     title: 'Woman’s Pink Sleepwear',
     price: '180$',
     image: 'images/home-images/Woman`s-pink-sleepwear.png',
+    id: '7',
 }
 ];
 
-export const detData = () => data;
+export const getData = () => data;
